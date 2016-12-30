e8f82cf01661cfbca7f9096dbe10a869c96592a3
Abhishek Verma <abhishek.verma03@snapdeal.com>
2016-12-30 14:40:03 +0530

    fixed test cases

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -12,7 +12,7 @@ def pg(event_loop):
 
 @pytest.mark.asyncio
 async def test_pool(pg):
-    pool = await pg.create_pool()
+    pool = await pg.get_pool()
     con = await pool.acquire()
     result = await con.fetch('SELECT * FROM sqrt(16)')
     assert result[0]['sqrt'] == 4.0
@@ -21,7 +21,7 @@ async def test_pool(pg):
 
 @pytest.mark.asyncio
 async def test_pool_connection_transaction_context_manager(pg):
-    pool = await pg.create_pool()
+    pool = await pg.get_pool()
     async with pool.acquire() as con:
         async with con.transaction():
             result = await con.fetchrow('SELECT * FROM sqrt(16)')
