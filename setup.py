53bc64c2ef78da39cb41e7a277e7338d27bb0952
Abhishek Verma <ashuverma1989@gmail.com>
2017-03-06 17:17:31 +0530

    version updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='trelliopg',
-      version='0.0.18b',
+      version='0.0.18',
       author='Abhishek Verma, Nirmal Singh',
       author_email='ashuverma1989@gmail.com, nirmal.singh.cer08@itbhu.ac.in',
       url='https://github.com/technomaniac/trelliopg',
