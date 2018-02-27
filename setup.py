717e80293577ec4c8fd5de816f871c387ecb6457
Abhishek Verma <ashuverma1989@gmail.com>
2018-02-27 20:12:51 +0530

    Added support for seach query

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='trelliopg',
-      version='0.0.28',
+      version='0.0.29rc1',
       author='Abhishek Verma, Nirmal Singh',
       author_email='ashuverma1989@gmail.com, nirmal.singh.cer08@itbhu.ac.in',
       url='https://github.com/zoniclabs/trelliopg',
